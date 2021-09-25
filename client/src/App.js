278a4f72e7b1a7632065d5862675e17eb6bd483c
Maxim Petrenko <maksapetrenko@gmail.com>
2021-09-25 19:10:43 +0300

    linter tamed, several rules disabled inline and a bunch - in a lock file

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import AppRouter from './components/AppRouter';
+import AppRouter from './components/AppRouter.js';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,6 +1,6 @@
-import Cart from '../pages/Cart/Cart';
+import Cart from '../pages/Cart/Cart.js';
 import Main from '../pages/Main/Main';
-import Admin from '../pages/Admin/Admin';
+import Admin from '../pages/Admin/Admin.js';
 import Products from '../pages/Products/Products';
 import Product from '../pages/Product/Product';
 
