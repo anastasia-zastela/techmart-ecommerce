998b2d861f1b5b4dfeeac0ce0162955038863291
anastasia_zastela <anastasia.zastela@gmail.com>
2021-09-29 17:06:22 +0300

    upload images to cloudinary added, travis.yml fixed

--- utils/multer.js
+++ utils/multer.js
@@ -1,7 +1,14 @@
-const cloudinary = require('cloudinary').v2;
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+const multer = require('multer');
+const path = require('path');
+// Multer config
+module.exports = multer({
+  storage: multer.diskStorage({}),
+  fileFilter: (req, file, cb) => {
+    let ext = path.extname(file.originalname);
+    if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+      cb(new Error('File type is not supported'), false);
+      return;
+    }
+    cb(null, true);
+  },
 });
-module.exports = cloudinary;
