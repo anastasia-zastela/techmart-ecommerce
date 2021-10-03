c4cb3bbd1098bc24f000a4807593c181828abb1e
anastasia_zastela <anastasia.zastela@gmail.com>
2021-10-03 19:12:10 +0300

    fixes after feedback

--- client/src/pages/Main/index.jsx
+++ client/src/pages/Main/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
 import ProductList from '../../components/common/ProductList/ProductList';
-import { useStyles } from './styles';
+import useStyles from './styles';
 import { products } from './products';
 
 const Main = () => {
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,5 +1,5 @@
 import Cart from '../pages/Cart/Cart.js';
-import Main from '../pages/Main/Main';
+import Main from '../pages/Main';
 import Admin from '../pages/Admin/Admin.js';
 import Products from '../pages/Products/Products';
 import Product from '../pages/Product/Product';
@@ -43,7 +43,7 @@ export const publicRoutes = [
     exact: true,
   },
   {
-    path: PRODUCTS_ROUTE + '/:id',
+    path: `${PRODUCTS_ROUTE}/:id`,
     component: Product,
     exact: true,
   },
