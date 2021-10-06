4d5ab5507be3ddf646c47060d18bce5228c253bb
Maxim Petrenko <maksapetrenko@gmail.com>
2021-10-06 20:35:20 +0300

    Signin Page ready

--- client/src/pages/Signin/SigninForm/component.jsx
+++ client/src/pages/Signin/SigninForm/component.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Form, Formik } from 'formik';
+
+import TextfieldWrapper from '../../../components/common/forms/TextfieldWrapper';
+import SubmitBlock from './SubmitBlock';
+import { useActions } from '../../../hooks/useActions';
+import { useStyles } from './styles';
+import {
+  signinFieldsInitData as signinFormFieldsData,
+  signinValidationSchema,
+} from './constants';
+
+const initialValues = signinValidationSchema.default();
 
 const SigninForm = () => {
+  const { login } = useActions();
+  const submitHandler = useCallback(async (values) => login(values), [login]);
+  const classes = useStyles();
 
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={submitHandler}
+      validationSchema={signinValidationSchema}
+      validateOnChange={false}
+    >
+      {({ isSubmitting }) => (
+        <Form className={classes.formBody}>
+          {signinFormFieldsData
+            .map((fieldParams) => (
+              <TextfieldWrapper
+                key={fieldParams.label}
+                className={classes.formInput}
+                {...fieldParams}
+              />
+            ))}
+          <SubmitBlock buttonClass={classes.submitButton} isSubmitting={isSubmitting} />
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export default SigninForm;
--- client/src/pages/Signin/component.jsx
+++ client/src/pages/Signin/component.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { Link } from '@material-ui/core';
 import SigninForm from './SigninForm';
+import { useStyles } from './styles';
+import { useActions } from '../../hooks/useActions';
+
+const userLoginSelector = (state) => state.userLogin;
 
 const Signin = () => {
-  const { userInfo } = useSelector((state) => state.userLogin);
+  const { userInfo } = useSelector(userLoginSelector);
+  const { logout } = useActions();
+  const classes = useStyles();
 
   return (
-    userInfo
-      ? <p>You seem to be already signed in</p>
-      : <SigninForm />
+    <section className='signin-page'>
+      <div className={classes.wrapper}>
+        {userInfo
+          ? (
+            <>
+              <h3>Вы уже представились системе</h3>
+              <p>
+                Для выхода из аккаунта&nbsp;
+                <Link className={classes.link} onClick={logout}>нажмите здесь</Link>
+              </p>
+            </>
+          ) : (
+            <>
+              <h2 className={classes.title}>Авторизация</h2>
+              <SigninForm />
+            </>
+          )}
+      </div>
+    </section>
   );
 };
 
