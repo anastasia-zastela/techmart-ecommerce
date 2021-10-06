1a82bb3c6cd6ae0c5c5c1860938bcf21eb14d388
Pavlo Klymchuk <pavlo95klymchuk@gmail.com>
2021-10-06 20:06:30 +0300

    add scroll

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,19 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import NavBar from './components/NavBar/NavBar.js';
 import './styles.css';
+import SimpleBarReact from 'simplebar-react';
+import "simplebar/src/simplebar.css";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Header />
-            <NavBar />
-            <AppRouter />
-            <Footer />
-        </BrowserRouter>
+        <SimpleBarReact style={{ height: '100vh', width: '100%' }}>
+            <BrowserRouter>
+                <Header />
+                <NavBar />
+                <AppRouter />
+                <Footer />
+            </BrowserRouter>
+        </SimpleBarReact>
     );
 }
 
