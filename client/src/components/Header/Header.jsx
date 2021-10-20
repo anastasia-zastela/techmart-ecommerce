d2f0d5b3ed27ce5ac1b6094043c62f2d8c8df0d0
Pavlo Klymchuk <pavlo95klymchuk@gmail.com>
2021-10-20 19:30:35 +0300

    fixed id

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -43,17 +43,17 @@ const Header = () => {
                                 placeholder='Поиск'
                                 variant="outlined"
                                 onChange={(e) => setSearchInput(e.target.value)}
-                                id={'seachHeader'}
+                                id='seachHeader'
                             />
                         </Grid>
                         <Grid item xs={3} sm={2} md={4}>
-                            <button className={classes.buttonSearch} onClick={searchForProducts} id={'buttonSeachHeader'}><span>Поиск</span></button>
+                            <button className={classes.buttonSearch} onClick={searchForProducts} id='buttonSeachHeader'><span>Поиск</span></button>
                         </Grid>
                     </Box>
                 </Grid>
                 <Grid item xs={3} sm={2} md={1} className={classes.gridCart}>
                     <Button color="inherit">
-                        <NavLink to="/cart" id={'cartHeader'}>
+                        <NavLink to="/cart" id='cartHeader'>
                             <svg width="35" height="35" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke='white' strokeLinecap="round" strokeLinejoin="round" className={classes.shoppingCart}>
                                 <path d="M9 22C9.55228 22 10 21.5523 10 21C10 20.4477 9.55228 20 9 20C8.44772 20 8 20.4477 8 21C8 21.5523 8.44772 22 9 22Z" />
                                 <path d="M20 22C20.5523 22 21 21.5523 21 21C21 20.4477 20.5523 20 20 20C19.4477 20 19 20.4477 19 21C19 21.5523 19.4477 22 20 22Z" />
