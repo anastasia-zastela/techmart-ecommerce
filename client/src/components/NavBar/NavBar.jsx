93f0cb5369bbc81ab1db0b82e72bfd0098051687
Pavlo Klymchuk <pavlo95klymchuk@gmail.com>
2021-10-29 17:47:44 +0300

    add aboutus and contects links

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -5,6 +5,7 @@ import {
     Button,
     Menu,
     MenuItem,
+    Grid,
 } from "@material-ui/core";
 import { NavLink } from "react-router-dom";
 import { useStyles } from './styles';
@@ -24,20 +25,44 @@ const NavBar = () => {
     return (
         <>
             <Toolbar className={classes.NavBar} position="static">
-                <Button color="inherit" >
-                    <NavLink to="/" className={classes.linkNavBar}>
-                        Главная
-                    </NavLink>
-                </Button>
-                <Button
-                    aria-controls='menu'
-                    onClick={handleOpenMenu}
-                    color="inherit"
+                <Grid
+                    container
+                    spacing={2}
+                    style={{ margin: 0, width: '100%', alignItems: 'center' }}
                 >
-                    <Typography className={classes.linkNavBar}>
-                        Меню
-                    </Typography>
-                </Button>
+                    <Grid item xs={6} sm={3} md={3} style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button color="inherit" >
+                            <NavLink to="/" className={classes.linkNavBar}>
+                                Главная
+                            </NavLink>
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6} sm={3} md={3} style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button
+                            aria-controls='menu'
+                            onClick={handleOpenMenu}
+                            color="inherit"
+                        >
+                            <Typography className={classes.linkNavBar}>
+                                Меню
+                            </Typography>
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6} sm={3} md={3} style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button color="inherit" >
+                            <NavLink to="/aboutus" className={classes.linkNavBar}>
+                                О нас
+                            </NavLink>
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6} sm={3} md={3} style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button color="inherit" >
+                            <NavLink to="/contacts" className={classes.linkNavBar}>
+                                Контакты
+                            </NavLink>
+                        </Button>
+                    </Grid>
+                </Grid>
             </Toolbar>
             <Menu
                 className={classes.menu}
