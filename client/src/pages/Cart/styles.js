ec967025e273a5431983cb67b274373528b15c39
Rostyslav Ozhihov <rostyslav.ozhihov@customertimes.com>
2021-11-05 14:24:48 +0200

    [Fix]{Page]:Cart

--- client/src/pages/Cart/styles.js
+++ client/src/pages/Cart/styles.js
@@ -1,24 +1,24 @@
 import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles(() => ({
-
-    cartTitle: {
-        padding: "10px 34px",
-        color: "rgb(0, 0, 0)",
-    },
-    cartButton: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        margin: '20px 0',
-    },
-    btnChekout: {
-        backgroundColor: 'rgb(62, 192, 221)',
-        marginTop: '20px'
-    },
-    cartItems:{
-        marginBottom: '50px'
-    }
+  cartTitle: {
+    padding: "10px 34px",
+    color: "rgb(0, 0, 0)",
+  },
+  cartButton: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "20px 0",
+  },
+  btnChekout: {
+    color: "rgb(62, 192, 221)",
+    background: "white",
+    border: "1px solid rgb(62, 192, 221)",
+    marginTop: "20px",
+  },
+  cartItems: {
+    marginBottom: "50px",
+  },
 }));
-
