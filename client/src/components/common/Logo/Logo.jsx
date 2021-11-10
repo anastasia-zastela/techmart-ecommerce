1808b65f0783a8d7c75a25a6a8f3e3b5aebf867f
maxim-petrenko <87077743+maxim-petrenko@users.noreply.github.com>
2021-11-10 19:36:56 +0200

    Fix/icons display (#36)

--- client/src/components/common/Logo/Logo.jsx
+++ client/src/components/common/Logo/Logo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Typography } from '@material-ui/core';
 import { useStyles } from './styles';
+import logoSVG from './Triangle-Logo.svg';
 
 const Logo = () => {
     const classes = useStyles();
@@ -9,7 +10,7 @@ const Logo = () => {
         <NavLink to='/' className={classes.logo} id='logo'>
             <img
                 alt='logo'
-                src='./images/Triangle-Logo.svg'
+                src={logoSVG}
                 className={classes.img}
             />
             <Typography variant='h6' className={classes.title}>
