ede8e465fb2bb43bd954164769e9a4debfd18a64
Maxim Petrenko <maksapetrenko@gmail.com>
2021-11-11 20:26:03 +0200

    fixed coverage collection; set treshholds

--- client/src/components/common/forms/AuthForm/SubmitBlock/__tests__/component.test.jsx
+++ client/src/components/common/forms/AuthForm/SubmitBlock/__tests__/component.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import SubmitBlock from '../component';
+import SubmitBlock from '../component.jsx';
 
 let mockUserRequestState = {};
 jest.mock('react-redux', () => ({
