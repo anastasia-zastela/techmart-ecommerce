acb219ed073defea91143a5c48f7469eec590df3
Maxim Petrenko <maksapetrenko@gmail.com>
2021-11-11 21:30:41 +0200

    added a etst refactor example

--- client/src/components/common/Slider/__tests__/component.test.jsx
+++ client/src/components/common/Slider/__tests__/component.test.jsx
@@ -1,15 +1,28 @@
-import { render } from '@testing-library/react';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Slider from '../Slider';
 
-import { Carousel } from 'react-responsive-carousel';
+const imagesMock = ['./img1.png', './img2.png'];
 
 describe('Test Product', () => {
-  it('Smoke test for Slider', () => {
-    render(<Carousel />);
+  it('renders (smoke test)', () => {
+    render(<Slider images={imagesMock} />);
+
+    expect(screen.queryByLabelText('slider')).toBeInTheDocument();
   });
 
-  it('Slider to be defined', () => {
-    render(<Carousel />);
-    const slider = document.querySelector('.carousel-root');
-    expect(slider).toBeDefined();
+  it('doesn`t render if not provided with images', () => {
+    render(<Slider />);
+
+    expect(screen.queryByLabelText('slider')).not.toBeInTheDocument();
+  });
+
+  it('renders slides with provided images', () => {
+    render(<Slider images={imagesMock} />);
+
+    expect(screen.queryAllByRole('img').length)
+      .toBe(imagesMock.length ** 2);
+    expect(screen.queryAllByLabelText('slide item', { exact: false }).length)
+      .toBe(imagesMock.length);
   });
 });
